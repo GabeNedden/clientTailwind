76b268fd591aa160e176d7e9d89349139666bb61
GabeNedden <gabenedden@gmail.com>
2021-05-09 13:07:51 -0400

    Add header background in CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <div class="font-construct mb-8">
-        <header class="bg-hero-pattern bg-cover h-screen w-full flex justify-center border-b-8 border-white">
+        <header class="bg-cover h-screen w-full flex justify-center border-b-8 border-white">
             <div class="max-w-4xl w-full">
                 <div class="flex items-center justify-between text-gray-400">
                     <img class='object-contain mx-auto mt-8 h-36 w-full' src='https://img1.wsimg.com/isteam/ip/6e52cc35-1234-4fad-8162-6aa30e890f62/Groupe%20Constructek%20Logos.png/:/rs=w:373,h:162,cg:true,m/cr=w:373,h:162/qt=q:95' alt='logo'></img>
