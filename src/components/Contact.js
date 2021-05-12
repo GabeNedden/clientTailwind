7ee49903101bed04a84e73010c562731be4cc6eb
Workshop <gabenedden@gmail.com>
2021-05-12 10:21:08 -0400

    Contact: Contact in prog

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ import truck from '../images/truck.png';
 
 function Contact() {
     return (
+
         <div class="font-construct items-center justify-between">
             <div class="font-construct text-center block my-8 text-4xl md:text-6xl leading-tight font-bold text-black">Contact Us!</div>
                 <div class="w-10/12 mx-auto flex flex-wrap overflow-hidden">
@@ -24,6 +25,41 @@ function Contact() {
                         Contact Us
                     </button>
             </div>
+
+            <div class="bg-white dark:bg-gray-900">
+                <div class="flex w-full justify-around items-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white py-5">
+                </div>
+                <form action="" class="my-20 p-10 max-w-xl mx-auto shadow-md sm:border-0 md:border md:border-gray-900 md:dark:border-gray-100 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100">
+                    <div class="mb-10">
+                        <h1 class="font-bold text-4xl mb-3">Get in touch</h1>
+                        <p class="font-medium text-lg mb-5">Send us a quick message and we'll get back to you shortly.</p>
+                        <hr class="border-gray-900 dark:border-gray-100" />
+                    </div>
+                    <div class="mb-5">
+                        <label for="message" class="text-lg flex justify-between items-end"><span>Message</span><span class="text-xs text-red-500">Required</span></label>
+                        <textarea name="message" id="message" cols="30" rows="10" class="shadow-md mt-1 block w-full sm:text-sm rounded-none border-gray-900 dark:border-gray-100 bg-white dark:bg-gray-900"></textarea>
+                    </div>
+                    <div class="mb-5">
+                        <label for="name" class="text-lg flex justify-between items-end"><span>Name</span><span class="text-xs text-red-500 ">Required</span></label>
+                        <div class="mt-1 flex shadow-md">
+                            <span class="inline-flex items-center px-3 rounded-none border border-r-0 border-gray-900 dark:border-gray-100"><i class="fas fa-user"></i></span>
+                            <input type="text" name="name" class="flex-1 block w-full sm:text-sm rounded-none border border-gray-900 dark:border-gray-100 bg-white dark:bg-gray-900" />
+                        </div>
+                    </div>
+                    <div class="mb-5">
+                        <label for="email" class="text-lg flex justify-between items-end"><span>Email</span><span class="text-xs text-red-500">Required</span></label>
+                        <div class="mt-1 flex shadow-md">
+                            <span class="inline-flex items-center px-3 rounded-none border border-r-0 border-gray-900 dark:border-gray-100"><i class="fas fa-envelope"></i></span>
+                            <input type="email" name="email" class="flex-1 block w-full sm:text-sm rounded-none border border-gray-900 dark:border-gray-100 bg-white dark:bg-gray-900" />
+                        </div>
+                    </div>
+                    <div>
+                        <button type="button" class="font-medium shadow-md rounded-none p-2 w-full focus:outline-none focus:ring-2 focus:ring-offset-2 border border-gray-900 dark:border-gray-100 bg-gray-800 dark:bg-gray-200 text-gray-200 dark:text-gray-800 hover:bg-gray-900 dark:hover:bg-gray-100"><i class="fas fa-check"></i> Send</button>
+                    </div>
+                </form>
+                </div>
+
+
         </div>
     )
 }
