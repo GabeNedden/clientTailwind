979ea8a543c9495b5aadb3882c3697e1e064ead9
Workshop <gabenedden@gmail.com>
2021-05-13 10:46:02 -0400

    Captcha test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -31,7 +31,7 @@ function Contact() {
                 <div class="flex w-full justify-around items-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white py-5">
                 </div>
 
-                <form name="simpleContactForm" method="POST" data-netlify="true" id="simple-contact-form" class="my-20 p-10 max-w-xl mx-auto shadow-md sm:border-0 md:border md:border-gray-900 md:dark:border-gray-100 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100">
+                <form name="simpleContactForm" method="POST" data-netlify-recaptcha="true" data-netlify="true" id="simple-contact-form" class="my-20 p-10 max-w-xl mx-auto shadow-md sm:border-0 md:border md:border-gray-900 md:dark:border-gray-100 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100">
                     <div class="mb-10">
                         <h1 class="font-bold text-4xl mb-3">Get in touch</h1>
                         <p class="font-medium text-lg mb-5">Send us a message and we'll get back to you shortly.</p>
@@ -56,6 +56,7 @@ function Contact() {
                             <input type="email" name="email" class="flex-1 block w-full sm:text-sm rounded-none border border-gray-900 dark:border-gray-100 bg-white dark:bg-gray-900" />
                         </div>
                     </div>
+                    <div data-netlify-recaptcha="true"></div>
                     <div>
                         <button type="submit" class="font-medium shadow-md rounded-none p-2 w-full focus:outline-none focus:ring-2 focus:ring-offset-2 border border-gray-900 dark:border-gray-100 bg-gray-800 dark:bg-gray-200 text-gray-200 dark:text-gray-800 hover:bg-gray-900 dark:hover:bg-gray-100"><i class="fas fa-check"></i> Send</button>
                     </div>
